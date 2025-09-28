4b83d2110c472c15eabe0e57cb2487c4aae40a6a
Lucky Satria <luckysatriautama2007@kamu.com>
2025-09-28 23:15:21 +0700

    Baru1

--- Test.java
+++ Test.java
@@ -2,6 +2,6 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama");
-        
+        System.out.println("Ini adalah baris ke dua");
     }
 }
